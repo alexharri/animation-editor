5f1430df761385208e8b58242a1a6a3dd3576534
alexharri <alex@taktikal.is>
2020-07-12 21:45:23 +0000

    mild refactor

--- src/util/color/convertColor.ts
+++ src/util/color/convertColor.ts
@@ -3,55 +3,52 @@ import { RGBColor, HSLColor } from "~/types";
 export const rgbToHSL = (rgbColor: RGBColor): HSLColor => {
 	let [r, g, b] = rgbColor;
 
-	// Make r, g, and b fractions of 1
+	// 0-1
 	r /= 255;
 	g /= 255;
 	b /= 255;
 
-	// Find greatest and smallest channel values
 	const cmin = Math.min(r, g, b);
 	const cmax = Math.max(r, g, b);
+
 	const delta = cmax - cmin;
+
 	let h = 0;
-	let s = 0;
-	let l = 0;
+	let l = (cmax + cmin) / 2;
+	let s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
 
 	// Calculate hue
-	// No difference
-	if (delta === 0) h = 0;
-	// Red is max
-	else if (cmax === r) h = ((g - b) / delta) % 6;
-	// Green is max
-	else if (cmax === g) h = (b - r) / delta + 2;
-	// Blue is max
-	else h = (r - g) / delta + 4;
+	if (delta === 0) {
+		h = 0;
+	} else if (cmax === r) {
+		h = ((g - b) / delta) % 6;
+	} else if (cmax === g) {
+		h = (b - r) / delta + 2;
+	} else {
+		h = (r - g) / delta + 4;
+	}
 
 	h = Math.round(h * 60);
 
-	// Make negative hues positive behind 360°
-	if (h < 0) h += 360;
+	if (h < 0) {
+		h += 360;
+	}
 
-	// Calculate lightness
-	l = (cmax + cmin) / 2;
-
-	// Calculate saturation
-	s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-
-	// Multiply l and s by 100
-	s = +(s * 100).toFixed(1);
-	l = +(l * 100).toFixed(1);
+	s = Number((s * 100).toFixed(1));
+	l = Number((l * 100).toFixed(1));
 
 	return [h, s, l];
 };
 
 export const hslToRGB = ([h, s, l]: HSLColor): RGBColor => {
-	// Must be fractions of 1
+	// 0-1
 	s /= 100;
 	l /= 100;
 
 	const c = (1 - Math.abs(2 * l - 1)) * s;
 	const x = c * (1 - Math.abs(((h / 60) % 2) - 1));
 	const m = l - c / 2;
+
 	let r = 0;
 	let g = 0;
 	let b = 0;
@@ -81,6 +78,7 @@ export const hslToRGB = ([h, s, l]: HSLColor): RGBColor => {
 		g = 0;
 		b = x;
 	}
+
 	r = Math.round((r + m) * 255);
 	g = Math.round((g + m) * 255);
 	b = Math.round((b + m) * 255);
